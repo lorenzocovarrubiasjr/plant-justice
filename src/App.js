1b69e8267ea260688ead18ca30486571bcee2626
witeoutmysin <lorenzocovarrubiasjr@gmail.com>
2019-11-18 14:44:57 -0600

    added Header component and styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component.jsx'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div>
-      <h1>Plant Justice</h1>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
