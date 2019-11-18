7817cdcc0eae87d3ddf4a208289561797a1cc26c
witeoutmysin <lorenzocovarrubiasjr@gmail.com>
2019-11-18 12:23:37 -0600

    Added React-Router capabilites and onClick to Homepage menu items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
       <h1>Plant Justice</h1>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
